927db6056e2d747031e85d522268c54a567d90e8
SaraWestWA <64798747+SaraWestWA@users.noreply.github.com>
2021-01-06 18:07:45 -0800

    functional model

--- app/ml.py
+++ app/ml.py
@@ -1,26 +1,30 @@
 """Machine learning functions"""
 
 import logging
+import joblib
 import random
+import pandas as pd
 
 from fastapi import APIRouter
 import pandas as pd
 from pydantic import BaseModel, Field, validator
+from sklearn.neighbors import NearestNeighbors
+
+
 
 log = logging.getLogger(__name__)
 router = APIRouter()
 
 class User(BaseModel):
     """Use this data model to parse the request body JSON."""
-    User_Input: str = Field(..., example=
-        """I am having trouble sleeping, I'm stressed and I don't want to eat.
-        I would like to have an appetite, sleep well, and be able to relax.""")
+    user_input: str = Field(..., example=
+        """I am having trouble sleeping, I'm stressed and I don't want to eat. I would like to have an appetite, sleep well, and be able to relax.""")
 
 
     def to_df(self):
         """Convert pydantic object to pandas dataframe with 1 row."""
         return pd.DataFrame([dict(self)])
-\
+
 
 @router.post('/predict')
 async def predict(user: User):
@@ -29,18 +33,31 @@ async def predict(user: User):
 
     ### Request Body
 
-    - `User_Input:`: string
+    - `user_input:`: string
 
     ### Response
-    - `stain_name`: string
+    - `strain_name`: string
     - `description`: string
-    - `strain_ailment`: string
-    - `strain_effect`: string
+    - `strain_effects`: string
+    - `strain_ailments`: string
     """
+    
+    vectorizer = joblib.load('app/jl_tfidf.joblib')
+    nn = joblib.load('app/jl_knn.joblib')
+
+    MJ = pd.read_csv('data\MJ.csv')
+
+    model_food = vectorizer.transform([user.user_input])
+    strain_name_v=(MJ.loc[nn.kneighbors(model_food.todense())[1][0][0]]['Strain']),
+    description_v=(MJ.loc[nn.kneighbors(model_food.todense())[1][0][0]]['Description']),
+    strain_effects_v=(MJ.loc[nn.kneighbors(model_food.todense())[1][0][0]]['Effects_y']),
+    strain_ailments_v=(MJ.loc[nn.kneighbors(model_food.todense())[1][0][0]]['Ailment'])
+
     return {
-        'strain_name': 'nice pot',
-        'description': 'lots of cool info about nice pot',
-        'strain_ailments': 'insomnia, anxiety',
-        'strain_effects': 'peace, chillness'
+
+        'strain_name': strain_name_v,
+        'description': description_v,
+        'strain_effects': strain_effects_v,
+        'strain_ailments': strain_ailments_v
         }
 
