d9ff3d93570478abc9dcfc324e8b6d5d4d480735
Timothy Eakin <61610170+DoctorDroid@users.noreply.github.com>
2021-01-10 20:01:53 -0500

    finalized with processed model food (input)

--- app/model.py
+++ app/model.py
@@ -66,7 +66,7 @@ async def predict(user: User):
     # stemmed = ""
     # for item in output:
     #     stemmed += item + " "
-    # model_food = vectorizer.transform(stemmed)
+    # model_food = vectorizer.transform([stemmed])
 
 ############################
 
